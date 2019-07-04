945ff3cb6e05fda6dbb040841e7e15d88d0f4d09
coderhook <abel1987@hotmail.es>
2019-07-04 22:32:10 +0200

    update

--- index.js
+++ index.js
@@ -9,21 +9,7 @@ const advertisementRoutes = require('./advertisement/routes')
 const app = express()
 const jsonParser = bodyParser.json()
 
-const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
-
-app.use(allowCrossDomain)
+app.use(cors())
 app.use(jsonParser)
 app.use(advertisementRoutes)
 
